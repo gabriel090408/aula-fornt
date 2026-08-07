b16e0bba9004fcd44cde525f54d5565a8e5335b7
gabriel090408 <gabi.sundl@gmail.com>
2026-08-06 21:34:33 -0300

    AULA

---
+++ listaFuncao.js
@@ -0,0 +1,96 @@
+//function listaIndex(lista=[]){
+//const multi = []
+//for (let i = 0; i < lista.length; i++) {
+  //  multi.push(lista[i] * i)
+
+
+//}
+//return multi    
+
+//}
+
+//const resposta = listaIndex([1,3,4,5,89,56,5])
+//console.log(resposta)
+
+
+
+
+
+//function listaContrario(lista=[]){
+    //const inverso = []
+  //  for (let i = lista.length - 1; i >= 0; i--) {
+//        inverso.push(lista[i])
+
+  //  }  
+  //  return inverso
+
+//}
+//const resposta = listaContrario(["abacate", "banana", "laranja", "morango"])
+//console.log(resposta)
+
+
+//function palavraA (palavra=[]){
+ //   const letraA = []
+  //  for (let i = 0; i < palavra.length; i++) {
+        
+        
+        
+    //    if (palavra[i][0].toLowerCase() ===("a")) {
+      //      letraA.push(palavra[i])
+        //}
+    //}
+    
+    //return letraA
+
+//}   
+//const resposta = palavraA(["Abacate", "banana", "laranja", "morango"])
+//console.log(resposta)
+
+
+
+
+  //  const resultado = [] {function listaContrario(lista=[]){
+    //const inverso = []
+    //for (let i = lista.length - 1; i >= 0; i--) {
+     //   inverso.push(lista[i])
+
+    //}  
+   // return inverso
+
+//}
+//const resposta = listaContrario(["abacate", "banana", "laranja", "morango"])
+//console.log(resposta)
+//} 
+
+//function imparPar(lista=[]){
+    //const resultado = []  
+
+    //for (let i = 0; i < lista.length; i++) {
+        //if (lista[i] % 2 === 0) {
+            //resultado.push("par")
+        //} else if (lista[i] % 3 === 0) { 
+          //  resultado.push("impar")
+      //  } else { resultado.push("shazam")}
+
+    //}
+  //  return resultado
+//} 
+//const resposta = imparPar([1,2,3,4,5,6,7,8,9,10])   
+//console.log(resposta)
+
+
+
+function listaSoma(lista=[]){
+const resultado = []
+for (let i = 0; i < lista.length; i++) { 
+    
+   
+
+}
+
+return resultado
+
+
+}
+const resposta = listaSoma([1,2,3,4,5,6,7,8,9,10])
+console.log(resposta)
